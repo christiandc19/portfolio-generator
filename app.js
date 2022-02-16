257bdf795caeef5b58edbb8583af1e55571b5c54
unknown <christiandc19@hotmail.com>
2022-02-15 23:58:10 -0800

    Added Project Questions

--- app.js
+++ app.js
@@ -1,18 +1,97 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
 
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+const promptUser = () => {
+  return inquirer.prompt([
+{
+  type: "input",
+  name: "name",
+  message: "What is your name?"
+},
 
-console.log(profileDataArgs);
+{
+  type: "input",
+  name: "github",
+  message: "Enter your Github Username"
+},
+{
+  type: "input",
+  name: "about",
+  message: "Provide some information about yourself:"
+}
+]);
+};
 
-const [name, github] = profileDataArgs;
+promptUser()
+.then(answers => console.log(answers));
 
-console.log(name, github);
 
-const pageHTML = generatePage(name, github);
 
-fs.writeFile("index.html", pageHTML, err => {
-  if(err) throw err;
-  console.log("Portfolio Complete! Check out index.html to see the output!")
-});
+const promptProject = () => {
+  console.log(`
+  
+  =================
+
+  Add a New Project
+
+  =================
+
+  `);
+
+return inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of your project?"
+},
+{
+  type: "input",
+  name: "description",
+  message: "Provide a description of the project (Required)"
+},
+{
+  type: "checkbox",
+  name: "languages",
+  message: "What did you build this project with? (Check all that apply)",
+  choices: ["JavaScript", "Html", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+},
+{
+  type: "checkbox",
+  name: "link",
+  message: "Enter the Github link to your project. (Required)"
+},
+{
+  type: "confirm",
+  name: "feature",
+  message: "Would you like to feature this project?",
+  default: false
+},
+{
+  type: "confirm",
+  name: "confirmAddProject",
+  message: "Would you like to enter another project?",
+  default: false
+}
+]);
+};
+
+
+promptUser()
+.then(answers => console.log(answers))
+.then(promptProject)
+.then(projectAnswers => console.log(projectAnswers));
+
+
+
+
+// const fs = require("fs");
+
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile("./index.html", pageHTML, err => {
+//   if(err) throw err;
+
+//   console.log("Portfolio Complete! Check out index.html to see the output!")
+// });
